842f0e40933fed95e421b35ee574c07990e960c3
kingxxasavan <78873531+kingxxasavan@users.noreply.github.com>
2025-10-29 10:45:36 -0400

    Update landing.py

--- landing.py
+++ landing.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-page_title=“CrypticX - AI Study Tool”,
+page_title=CrypticX - AI Study Tool”,
 page_icon=“⚡”,
 layout=“wide”,
 initial_sidebar_state=“collapsed”
